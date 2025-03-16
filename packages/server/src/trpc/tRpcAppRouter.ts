202cc60e7609979c66bfe7ec34dcb542a7548c54
Alexander Johansson <alexander@n1s.se>
2025-03-16 21:23:26 +0100

    bump

--- packages/server/src/trpc/tRpcAppRouter.ts
+++ packages/server/src/trpc/tRpcAppRouter.ts
@@ -1,8 +1,28 @@
+import { User } from '@clerk/express';
+import { z } from 'zod';
 import { t } from './trpc';
-import { greetingApi } from 'trpc/tRpcApis/greetingTRpcApi';
+import { protectedProcedure, publicProcedure } from './trpcProcedures';
+
+
+const getPrimaryEmail = (user: User | null) => {
+  return user?.primaryEmailAddress?.emailAddress ?? 'unknown';
+};
 
 export const tRpcAppRouter = t.router({
-  ...greetingApi,
+  greetPrivate: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .query(({ input, ctx }) => {
+      const email = getPrimaryEmail(ctx.user);
+      // console.log(' ctx.user email :::  ', email);
+      return `PRIVATE: Hello, ${input.name}!  Current email is ${email} `;
+    }),
+  greet: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(({ input, ctx }) => {
+      const email = getPrimaryEmail(ctx.user);
+      // console.log(' ctx.user email :::  ', email);
+      return `PUBLIC: Hello, ${input.name}!  Current email is ${email} `;
+    }),
 });
 
 export type TRpcAppRouter = typeof tRpcAppRouter;
